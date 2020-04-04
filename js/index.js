99b637e0476977f0c5f5cd4b1d05698d23901e01
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2020-04-04 12:14:41 +0200

    better error handling

--- js/index.js
+++ js/index.js
@@ -18,32 +18,40 @@ function getUrlParam(parameter) {
 const onError = function(data) {
     text.append("An error occurred. ");
     if(data !== "") {
-        let json = JSON.parse(data);
-        let message = json.message;
-        if(typeof message === "undefined") {
-            message = json["guild_id"][0];
-            if(typeof message !== "undefined") {
-                message =  data;
+        let message = "";
+        try {
+            let json = JSON.parse(data);
+            message = json.message;
+            if (typeof message === "undefined") {
+                message = json["guild_id"][0];
+                if (typeof message !== "undefined") { //when data unexpected json:
+                    message = data;
+                }
             }
+        } catch (e) { //when data not json:
+            message = data;
         }
-        text.append("Message: " + message);
+        text.append("Message: '" + message + "' ");
     }
 };
 
-
 const openInvite = function (data) {
-    const invite = JSON.parse(data)["instant_invite"];
-    if(typeof invite === "undefined") {
-        onError(data);
-    } else {
-        let new_element = document.createElement("div");
-        new_element.textContent = "Click here if the forwarding doesn't work: ";
-        let hyper_link = document.createElement("a");
-        hyper_link.textContent = invite;
-        hyper_link.href = invite;
-        new_element.append(hyper_link);
-        text.append(new_element);
-        window.location = invite;
+    try {
+        const invite = JSON.parse(data)["instant_invite"];
+        if(typeof invite === "undefined") {
+            onError(data);
+        } else {
+            let new_element = document.createElement("div");
+            new_element.textContent = "Click here if the forwarding doesn't work: ";
+            let hyper_link = document.createElement("a");
+            hyper_link.textContent = invite;
+            hyper_link.href = invite;
+            new_element.append(hyper_link);
+            text.append(new_element);
+            window.location = invite;
+        }
+    } catch(e) {
+        onError(e.toString());
     }
 };
 
@@ -52,8 +60,8 @@ if(guildId) {
     fetch(widget.replace("ID", guildId))
         .then(function(response) {
             response.text().then(openInvite);
-        }, function (response) {
-            response.text().then(onError);
+        }, function (error) {
+            error.text().then(onError);
         }
     );
 } else {
