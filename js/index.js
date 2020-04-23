d5574f9fb4cb7c6b3b111c7c29aca43141349d6c
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2020-04-23 12:11:23 +0200

    Fixed bug, when instant_invite == null

--- js/index.js
+++ js/index.js
@@ -2,52 +2,63 @@ const widget = "https://discordapp.com/api/guilds/ID/widget.json";
 const text = document.getElementById("text");
 text.textContent = ""; //remove javascript disabled text
 
-function getUrlVars() {
-    let vars = {};
-    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+
+function isUndefined(val) {
+    return typeof val === "undefined";
+}
+function getUrlVarsLowerCase() {
+    const vars = {};
+    window.location.href.toLocaleLowerCase().replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
         vars[key] = value;
     });
     return vars;
 }
 
-function getUrlParam(parameter) {
-    const val = getUrlVars()[parameter];
-    return typeof val === "undefined" || val === "" ? false : val;
+function getUrlParamIgnoreCase(parameter) {
+    const val = getUrlVarsLowerCase()[parameter.toLocaleLowerCase()];
+    return isUndefined(val) || val === "" ? false : val;
 }
 
+const guildId = getUrlParamIgnoreCase("id");
+
 const onError = function(data) {
-    text.append("An error occurred. ");
+    text.append("An error occurred. Please check if '" + guildId + "' is correct. ");
     if(data !== "") {
         let message = "";
         try {
-            let json = JSON.parse(data);
+            const json = JSON.parse(data);
             message = json.message;
-            if (typeof message === "undefined") {
+            if (isUndefined(message)) {
                 message = json["guild_id"][0];
-                if (typeof message === "undefined") { //when data unexpected json:
+                if (isUndefined(message)) { //when data unexpected json:
                     message = data;
                 }
             }
         } catch (e) { //when data not json:
             message = data;
         }
-        text.append("Message: '" + message + "' ");
+        text.append("Message: '" + message + "'");
     }
 };
 
 const openInvite = function (data) {
     try {
         const invite = JSON.parse(data)["instant_invite"];
-        if(typeof invite === "undefined") {
+        if(isUndefined(invite)) {
             onError(data);
+        } else if (invite === null) {
+            onError("instant_invite is null")
         } else {
-            let new_element = document.createElement("div");
+            const new_element = document.createElement("div");
             new_element.textContent = "Click here if the forwarding doesn't work: ";
-            let hyper_link = document.createElement("a");
+
+            const hyper_link = document.createElement("a");
             hyper_link.textContent = invite;
             hyper_link.href = invite;
+
             new_element.append(hyper_link);
             text.append(new_element);
+
             window.location = invite;
         }
     } catch(e) { //probably data != json
@@ -55,7 +66,6 @@ const openInvite = function (data) {
     }
 };
 
-const guildId = getUrlParam("id");
 if(guildId) {
     fetch(widget.replace("ID", guildId))
         .then(function(response) {
