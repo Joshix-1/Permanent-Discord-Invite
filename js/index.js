d48de049f76e25abc644bfc58b46fe3342bcb76d
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2020-04-23 23:03:13 +0200

    Added list with useful links (when guild_id valid)

--- js/index.js
+++ js/index.js
@@ -19,10 +19,17 @@ function getUrlParamIgnoreCase(parameter) {
     return isUndefined(val) || val === "" ? false : val;
 }
 
+function createLinkElement(text_and_link) {
+    const item = document.createElement("a");
+    item.textContent = text_and_link;
+    item.href = text_and_link;
+    return item;
+}
+
 const guildId = getUrlParamIgnoreCase("id");
 
 const onError = function(data) {
-    text.append("An error occurred. Please check if '" + guildId + "' is correct. ");
+    const defaultErrorMsg = "An error occurred. Please check if '" + guildId + "' is the correct id.";
     if(data !== "") {
         let message = "";
         try {
@@ -33,11 +40,35 @@ const onError = function(data) {
                 if (isUndefined(message)) { //when data unexpected json:
                     message = data;
                 }
+            } else if (json.code !== 10004) { //10004 == Unknown Guild
+                //guild id is probably fine && error is probably Widget Disabled:
+                const text2 = document.createElement("div");
+                text2.textContent = "Maybe try one of these links (only when you think the guild maybe listed there):";
+
+                const links = ["https://discordservers.me/servers/", "https://disboard.org/server/", "https://top.gg/servers/"];
+                const list = document.createElement("ul");
+
+                let i;
+                for (i = 0; i < links.length; i++) {
+                    const item = document.createElement("li");
+                    item.appendChild(createLinkElement(links[i] + guildId));
+                    list.appendChild(item);
+                }
+
+                text2.append(list);
+                text.append(text2);
+
+                const footer = document.createElement("footer");
+                footer.textContent = "Disclaimer: This page isn't affiliated with any of the websites listed above. I just chose them, because they were the only " + links.length
+                    + " I could find, that used the guild ids in their url. If you think that one service is missing tell me on GitHub."
+                text.append(footer);
             }
         } catch (e) { //when data not json:
             message = data;
         }
-        text.append("Message: '" + message + "'");
+        text.prepend(defaultErrorMsg + " Message: '" + message + "'");
+    } else {
+        text.append(defaultErrorMsg);
     }
 };
 
@@ -52,11 +83,7 @@ const openInvite = function (data) {
             const new_element = document.createElement("div");
             new_element.textContent = "Click here if the forwarding doesn't work: ";
 
-            const hyper_link = document.createElement("a");
-            hyper_link.textContent = invite;
-            hyper_link.href = invite;
-
-            new_element.append(hyper_link);
+            new_element.append(createLinkElement(invite));
             text.append(new_element);
 
             window.location = invite;
